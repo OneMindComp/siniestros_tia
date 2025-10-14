c1bd292d94eb630ff4168b8788c2e8b789731129
Edwin Rodriguez <errivera@indracompany.com>
2025-10-14 18:59:10 -0300

    actualizacion

--- src/ICore.Siniestro.Dominio/Entidades/Denuncio/DenuncioSoap.cs
+++ src/ICore.Siniestro.Dominio/Entidades/Denuncio/DenuncioSoap.cs
@@ -6,10 +6,35 @@ namespace ICore.Siniestro.Dominio.Entidades.Denuncio
     public class DenuncioSoap : DenuncioSiniestro
     {
         public override TipoSiniestro TipoSiniestro => TipoSiniestro.SOAP;
-        public Poliza Poliza { get; set; } = default!;
-        public Denunciante Denunciante { get; set; } = default!;
-        public Vehiculo Vehiculo { get; set; } = default!;
-        public Conductor Conductor { get; set; } = default!;
-        public Lesionado? Lesionado { get; set; }
+        public Poliza Poliza { get; private set; } = default!;
+        public Denunciante Denunciante { get; private set; } = default!;
+        public Vehiculo Vehiculo { get; private set; } = default!;
+        public Conductor Conductor { get; private set; } = default!;
+        public Lesionado? Lesionado { get; private set; }
+        private DenuncioSoap()
+        {
+        }
+        private DenuncioSoap(
+        Poliza poliza,
+        Denunciante denunciante,
+        Vehiculo vehiculo,
+        Conductor conductor,
+        Lesionado? lesionado)
+        {
+            Poliza = poliza;
+            Denunciante = denunciante;
+            Vehiculo = vehiculo;
+            Conductor = conductor;
+            Lesionado = lesionado;
+        }
+        public static DenuncioSoap Crear(
+        Poliza poliza,
+        Denunciante denunciante,
+        Vehiculo vehiculo,
+        Conductor conductor,
+        Lesionado? lesionado = null)
+        {
+            return new DenuncioSoap(poliza, denunciante, vehiculo, conductor, lesionado);
+        }
     }
 }
